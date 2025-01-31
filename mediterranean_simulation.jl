0f2cec60f86141c9476707a4fe533fa0bff28732
gsannino <gianmaria.sannino@gmail.com>
2025-01-31 11:55:31 +0100

    Update mediterranean_simulation.jl

--- mediterranean_simulation.jl
+++ mediterranean_simulation.jl
@@ -17,16 +17,16 @@
 
 # NOT WORKING ON XQUARTZ using GLMakie
 using Pkg
-Pkg.activate(".")
-Pkg.add("StyledStrings")
-Pkg.add("CairoMakie")
-Pkg.add("Oceananigans")
-Pkg.add("ClimaOcean")
-Pkg.add("CUDA")
-Pkg.resolve()
-Pkg.instantiate()
+#Pkg.activate(".")
+#Pkg.add("StyledStrings")
+#Pkg.add("CairoMakie")
+#Pkg.add("Oceananigans")
+#Pkg.add("ClimaOcean")
+#Pkg.add("CUDA")
+#Pkg.resolve()
+#Pkg.instantiate()
 #Pkg.gc()  # Rimuove i pacchetti inutilizzati
-Pkg.update()  # Aggiorna i pacchetti
+#Pkg.update()  # Aggiorna i pacchetti
 using CairoMakie
 using Oceananigans
 using Oceananigans.Grids
